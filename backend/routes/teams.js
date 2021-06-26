e5fc3127b8f3b80ffb6c039c5883132a1caa28ee
sharanya02 <sharanya02@rediffmail.com>
2021-06-26 11:32:09 +0530

    fixed linting issues

--- backend/routes/teams.js
+++ backend/routes/teams.js
@@ -3,26 +3,26 @@ const TeamController = require('../controllers/teams')
 const middlewares = require('../middlewares/auth')
 
 router.post('/create', middlewares.isLoggedIn, async (req, res) => {
-    const { teamName } = req.body
-    const response = await TeamController.createTeam(req.decoded.id, teamName)
-    res.status(response.code).send(response)
+  const { teamName } = req.body
+  const response = await TeamController.createTeam(req.decoded.id, teamName)
+  res.status(response.code).send(response)
 })
 
 router.post('/user/add', middlewares.isLoggedIn, async (req, res) => {
-    const { userId, teamId } = req.body
-    const response = await TeamController.addUser(userId, teamId)
-    res.status(response.code).send(response)
+  const { userId, teamId } = req.body
+  const response = await TeamController.addUser(userId, teamId)
+  res.status(response.code).send(response)
 })
 
 router.post('/delete', middlewares.isLoggedIn, async (req, res) => {
-    const { teamId } = req.body
-    const response = await TeamController.removeTeam(req.decoded.id, teamId)
-    res.status(response.code).send(response)
+  const { teamId } = req.body
+  const response = await TeamController.removeTeam(req.decoded.id, teamId)
+  res.status(response.code).send(response)
 })
 
 router.post('/user/delete', middlewares.isLoggedIn, async (req, res) => {
-    const { userId, teamId } = req.body
-    const response = await TeamController.removeUser(req.decoded.id, userId, teamId)
-    res.status(response.code).send(response)
+  const { userId, teamId } = req.body
+  const response = await TeamController.removeUser(req.decoded.id, userId, teamId)
+  res.status(response.code).send(response)
 })
 module.exports = router
